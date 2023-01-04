b695a593a081fe5ccccc575b31f88587e82e6547
Tukhtaev27 <108573349+Tukhtaev27@users.noreply.github.com>
2023-01-04 19:46:36 +0500

    typescript enums and interfaces added

---
+++ src/state/reducers/bankReducer.ts
@@ -0,0 +1,19 @@
+import { ActionType } from '../action-types';
+import { Action } from '../actions/index'
+
+const initialState = 0;
+
+const reducer = (state: number = initialState, action: Action) => {
+    switch(action.type) {
+        case ActionType.DEPOSIT:
+            return state + action.payload;
+        case ActionType.WITHDRAW:
+            return state - action.payload;
+        case ActionType.BANKRUPT:
+            return 0;
+        default:
+            return state
+    }
+}
+
+export default reducer
